e61ccd6d3035df6eb8a77a479aad01178bcd21c1
VIKASH MANDLOI <126850164+its-vikash07@users.noreply.github.com>
2024-05-08 09:14:46 +0530

    Add files via upload

---
+++ WithoutUseLambda.java
@@ -0,0 +1,23 @@
+package LambdaExpression;
+interface Drawable{
+
+    public void draw();
+}
+public class WithoutUseLambda {
+
+    public static void main(String[] args) {
+        
+        int width=10;
+        Drawable d = new Drawable(){
+
+            @Override
+            public void draw(){
+
+                System.out.println("Drawing"+"="+ width);
+
+            }
+        };
+        d.draw();
+    }
+    
+}
